7e52d04cd4b12172909be6abc5ead45c962b7d97
Shantanu Sutar <shantanusutar17@gmail.com>
2023-03-20 23:02:42 +0530

    google authorization setup for sign in

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,30 +6,48 @@ import {
   Input,
   VStack,
 } from "@chakra-ui/react";
-
+import { useState, useEffect } from "react";
 import Message from "./Components/Message";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "./firebase";
+
+const auth = getAuth(app);
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+};
+
 function App() {
+  const [user, setUser] = useState(false);
+
   return (
     <Box bg={"red.50"}>
-      <Container height={"100vh"} bg={"white"}>
-        <VStack h={"full"} padding="4">
-          <Button colorScheme={"red"} w={"full"}>
-            Logout
-          </Button>
-          <VStack h={"full"} w="full" overflowY={"auto"}>
-            <Message text="Sample Message" />
-            <Message user="me" text="Sample Message" />
+      {user ? (
+        <Container height={"100vh"} bg={"white"}>
+          <VStack h={"full"} padding="4">
+            <Button colorScheme={"red"} w={"full"}>
+              Logout
+            </Button>
+            <VStack h={"full"} w="full" overflowY={"auto"}>
+              <Message text="Sample Message" />
+              <Message user="me" text="Sample Message" />
+            </VStack>
+            <form style={{ width: "100%" }} action="">
+              <HStack>
+                <Input placeholder="Enter a message..." />
+                <Button colorScheme={"purple"} type="submit">
+                  Send
+                </Button>
+              </HStack>
+            </form>
           </VStack>
-          <form style={{ width: "100%" }} action="">
-            <HStack>
-              <Input placeholder="Enter a message..." />
-              <Button colorScheme={"purple"} type="submit">
-                Send
-              </Button>
-            </HStack>
-          </form>
+        </Container>
+      ) : (
+        <VStack justifyContent={"center"} h="100vh">
+          <Button>Sign in with Google</Button>
         </VStack>
-      </Container>
+      )}
     </Box>
   );
 }
